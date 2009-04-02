c550b0733340dba6d26e68ca7f37daf800fbfbbd
Pedro Alcocer <pealco@gmail.com>
2009-04-02 16:40:44 -0400

    This needs some cleaning up. Some numbers need to be de-hard coded. In working order, though.

--- sqd2mat.m
+++ sqd2mat.m
@@ -1,13 +1,23 @@
-function [brain, refs] = sqd2mat(file)
+function [brain, refs] = sqd2mat(file, interesting_chans)
 
 % MEG data channels: 0:156
 % MEG ref  channels: 157, 158, 159
 % Matlab data channels: 1:157
 % Matlab ref  channels: 158:160
 
-data_chans    = [0:10];
-trigger_chans = [162, 163, 164, 165, 167, 168, 169, 170];
-expected_triggers = 200;
+% These channels are loaded because they are the ones most likely to contain
+% evidence that a blink has occured. Automatic epoch rejection looks only at
+% these channels.
+front_chans    = [0, 41, 42, 83, 84, 107, 106, 105, 104, 103, 102, 101, 100, 62, 61, 24, 23];
+data_chans = [front_chans  interesting_chans];
+
+% These should be changed based on what triggers you are using in your
+% experiment. Use MEG160 notation, not MATLAB (i.e., 0 is the first
+% channel).
+trigger_chans = [162, 163, 164, 165, 166, 167, 168, 169];
+
+% This should be changed based on how many triggers you expect to find.
+expected_triggers = 100;
 
 info = sqdread(file, 'info');
 sqd_length = info.SamplesAvailable;
@@ -16,11 +26,10 @@ for channel = data_chans,
    data{channel+1} = zeros(1,sqd_length); % Preallocate memory
 end
 
-for channel = 1:length(data_chans)/2,
-    disp(['Reading channel ' num2str(channel*2-1) ' and ' num2str(channel*2) ' ...'])
-    current_channel = sqdread(file,'Channels',[((channel*2)-1)-1, (channel*2)-1]);
-    data{(channel*2)-1} = current_channel(:,1);
-    data{(channel*2)}   = current_channel(:,2);
+for channel = data_chans,
+    disp(['Reading channel ' num2str(channel) ' ...'])
+    current_channel = sqdread(file,'Channels',[channel]);
+    data{channel+1} = current_channel(:,1);
 end
 
 disp('Filtering and decimating ...')
