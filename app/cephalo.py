bc768cc8a43f8af784d0d2917c69150ba2c5bb01
Pedro Alcocer <pealco@gmail.com>
2009-11-17 17:35:17 -0500

    Bug fixes. Added features:  randomly selects epochs.

--- app/cephalo.py
+++ app/cephalo.py
@@ -4,7 +4,7 @@ import tables
 from lib.megprocess import *
 
 class Configuration():
-    def __init__(self, config_file):
+    def __init__(self):
         self.get_config()
     
     def get_config(self):
@@ -71,7 +71,7 @@ class Configuration():
                 raise ValueError("This is an MMF design and no standards or deviants are specified.")
             self.standards = config['standards']
             self.deviants = config['deviants']
-            self.trigger_channels = self.standards.keys() + self.deviants.keys()
+            self.trigger_channels = sorted(self.standards.keys() + self.deviants.keys())
             self.num_of_conditions = len(self.trigger_channels)
     
         if 'subjects' not in config or config['subjects'] == {}:
@@ -81,53 +81,77 @@ class Configuration():
 
 class Model():
     
+    def __init__(self, config):
+        self.config = config
+    
     def process(self, subject, channels_of_interest):
-        h5_file = self.data_directory + subject + ".h5"
+        h5_file = self.config.data_directory + subject + ".h5"
         
         print "Working on ", h5_file, "..."
-        data = load_data(h5_file, self.trigger_channels)
+        data = load_data(h5_file, self.config.trigger_channels)
         
         front_sensors = [0, 41, 42, 83, 84, 107, 106, 105, 104, 103, 102, 101, 100, 62, 61, 24, 23]
         loaded_channels = front_sensors + channels_of_interest
         
-        print "Filtering ..."
-        for c in range(157):
-            data.root.lowpass_data[c] = lowpass(data.root.raw_data[c] * data.root.convfactor[c], 1000, 20) 
+        if "/lowpass_data" not in data:
+            print "Filtering ..."
+            for c in range(157):
+                data.root.lowpass_data[c] = lowpass(data.root.raw_data[c] * data.root.convfactor[c], 1000, 20) 
         
-        epochs = epoch(data.root.raw_data, data.root.triggers, loaded_channels, range(self.num_of_conditions)) # Raw epochs.
-        epochs_filtered = epoch(data.root.lowpass_data, data.root.triggers, loaded_channels, range(self.num_of_conditions)) # Filtered epochs.
+        # Raw epochs.
+        epochs = epoch(data.root.raw_data, 
+                       data.root.triggers, 
+                       loaded_channels, 
+                       config=self.config) 
         
-        epochs = baseline(epochs.copy(), self.epochs_pre)
-        epochs_filtered = baseline(epochs_filtered.copy())
+        # Filtered epochs.
+        epochs_filtered = epoch(data.root.lowpass_data, 
+                                data.root.triggers, 
+                                loaded_channels, 
+                                config=self.config) 
+        
+        epochs = baseline(epochs.copy(), self.config.epoch_pre)
+        epochs_filtered = baseline(epochs_filtered.copy(), self.config.epoch_pre)
         
         save_epochs(data, epochs, epochs_filtered)
         
-        mean_epochs = zeros((self.num_of_conditions, shape(epochs)[1], len(channels_of_interest)))    
-        for c in range(8):
+        mean_epochs = zeros((self.config.num_of_conditions, shape(epochs)[1], len(channels_of_interest)))    
+        for c in range(self.config.num_of_conditions):
             accepted_epochs = reject_epochs(epochs[c, :, :, :], method="diff")
             rejected_epochs = list(set(range(shape(epochs[c,:,:,:])[2])) - set(accepted_epochs))
-
+            
             mean_epochs[c,:,:] = mean(epochs_filtered[c, :, len(front_sensors):, accepted_epochs], 0)
         
-        rms_mean_epochs = rms(mean_epochs, 2)
+        #rms_mean_epochs = rms(mean_epochs, 2)
         
         data.close()
         
-        return rms_mean_epochs
+        #return rms_mean_epochs
+        print "me_s", shape(mean_epochs)
+        return mean_epochs
         
     def analyze(self):
-        self.allsubjs = [self.process(subject, channels) for subject, channels in self.subjects.iteritems()]
-        self.grand_average = mean(self.allsubjs, axis=0)
-    
+        self.mean_epochs = [self.process(subject, channels) for subject, channels in self.config.subjects.iteritems()]
         
+        #self.allsubjs = [self.process(subject, channels) for subject, channels in self.config.subjects.iteritems()]
+        
+        #self.grand_average = mean(self.allsubjs, axis=0)
+        
+
 class View():
-    def plot_mmf(self, grand_average):
+    
+    def __init__(self, model):
+        self.model = model
+    
+    def plot_mmf(self):
         
-        for standard in self.model.standards:
+        for standard in self.model.config.standards:
             pass
         
-        for deviant in self.model.standards:
+        for deviant in self.model.config.deviants:
             pass
+        
+        grand_average = self.model.grand_average
         
         standard_4 = grand_average[0, :]  # dlif  standard
         deviant_3  = grand_average[1, :]  # delif deviant
@@ -137,15 +161,15 @@ class View():
         deviant_1  = grand_average[5, :]  # ledif deviant
         standard_1 = grand_average[6, :]  # ledif standard
         deviant_2  = grand_average[7, :]  # ldif  deviant
-
-
+        
+        
         difference_wave_1 = difference_wave(standard_1, deviant_1) # ledif
         difference_wave_2 = difference_wave(standard_2, deviant_2) # ldif 
         difference_wave_3 = difference_wave(standard_3, deviant_3) # delif
         difference_wave_4 = difference_wave(standard_4, deviant_4) # dlif
-
+        
         figure()
-
+        
         subplot(211)
         title("ledif")
         plot(standard_1, label="standard")
@@ -153,7 +177,7 @@ class View():
         #xlim(300, 400)
         #ylim(0, 70)
         legend()
-
+        
         subplot(212)
         title("ldif")
         plot(standard_2, label="standard")
@@ -161,9 +185,9 @@ class View():
         #xlim(300, 400)
         #ylim(0, 70)
         legend()
-
+        
         figure()
-
+        
         subplot(211)
         title("delif")
         plot(standard_3, label="standard")
@@ -171,7 +195,7 @@ class View():
         #xlim(300, 400)
         #ylim(0, 70)
         legend()
-
+        
         subplot(212)
         title("dlif")
         plot(standard_4, label="standard")
@@ -182,17 +206,44 @@ class View():
         
         show()
     
+    def output_for_r(self):
+        self.model.mean_epochs = array(self.model.mean_epochs)
+        subjects, conditions, samples, channels = shape(self.model.mean_epochs)
+        print shape(self.model.mean_epochs)
         
+        out = "subject condition sample channel value\n"
+        
+        for subject in xrange(subjects):
+            for condition in xrange(conditions):
+                for sample in xrange(samples):
+                    for channel in xrange(channels):
+                        out += "s%d c%d %d ch%d %f\n" % (subject+1, condition+1, sample-self.model.config.epoch_pre, channel+1, self.model.mean_epochs[subject, condition, sample, channel])
+                    
+        return out
+    
+
 class Experiment():
     def __init__(self):
-        self.model = Model()
-        self.view = View()
         
-        self.model.get_config()        
+        # Models
+        self.config = Configuration()
+        self.model = Model(self.config)
+        
         self.model.analyze()
         
-        self.view.plot_mmf(self.model.grand_average)
+        # Views
+        self.view = View(self.model)
+           
 
+        #self.view.plot_mmf()
+        output = self.view.output_for_r()
+        self.save("output.txt", output)
+        
+    def save(self, filename, contents):
+        fh = open(filename, 'w')
+        fh.write(contents)
+        fh.close()
+    
 if __name__ == "__main__":
     
     exp = Experiment()
--- app/lib/megprocess.py
+++ app/lib/megprocess.py
@@ -15,13 +15,24 @@ def load_data(h5_filename, trigger_channels):
     
     # Create CArray for lowpassed data.
     if "/lowpass_data" not in megdata:
-        lowpass_data = megdata.createCArray(megdata.root, 'lowpass_data', tables.Float32Atom(), shape(raw_data))
+        lowpass_data = megdata.createCArray(
+            where=megdata.root, 
+            name='lowpass_data', 
+            atom=tables.Float32Atom(), 
+            shape=shape(raw_data),
+            filters=tables.Filters(1)
+            )
     
     # Create and fill triggers VLArray. Allows for ragged rows.
     if "/triggers" in megdata:
         megdata.root.triggers.remove()
         
-    triggers = megdata.createVLArray(megdata.root, 'triggers', tables.Int32Atom(), filters=tables.Filters(1))
+    triggers = megdata.createVLArray(
+                   where=megdata.root, 
+                   name='triggers',
+                   atom=tables.Int32Atom(),
+                   filters=tables.Filters(1)
+                   )
     
     print "Finding triggers ..."
     for trigger_set in find_triggers(raw_data, trigger_channels):
@@ -41,24 +52,31 @@ def find_triggers(data, trigger_channels):
     
     return triggers
 
-def epoch(data, triggers, channels, conditions, max_epochs=100):
+def epoch(data, triggers, channels, config):
     """Pulls out the epochs from the long data file."""
     
-    stimulus_pre  = 100
-    stimulus_post = 500
-    epoch_length  = stimulus_pre + 1 + stimulus_post # + 1 for the actual stimulus, which occurs at 0
+    conditions = config.num_of_conditions
+    expected_epochs = config.expected_epochs
+    stimulus_pre = config.epoch_pre
+    stimulus_post = config.epoch_post
     
-    epochs = zeros((len(conditions), epoch_length, len(channels), max_epochs)) # Preallocate memory.
+    epoch_length  = stimulus_pre + 1 + stimulus_post # Add 1 for 0 point.
+    
+    epochs = zeros((conditions, epoch_length, len(channels), expected_epochs))
     
     for (channel_index, channel) in enumerate(channels):
         print "Epoching channel %s ..." % channel
         the_chan = data[channel][:]
-        for condition in conditions: 
-            for t in range(max_epochs):
+        for condition in range(conditions): 
+            epoch_indices = range(len(triggers[condition]))
+            random.shuffle(epoch_indices)
+            epoch_indices = epoch_indices[:expected_epochs]
+            
+            for i, t in enumerate(epoch_indices):
                 epoch_start = triggers[condition][t] - stimulus_pre
                 epoch_end   = triggers[condition][t] + stimulus_post
                 the_epoch = the_chan[range(epoch_start, epoch_end + 1)]
-                epochs[condition, :, channel_index, t] = the_epoch
+                epochs[condition, :, channel_index, i] = the_epoch
     
     ## This should work in the next version of pytables. Uses fancy indexing.   
     #for condition in conditions: 
@@ -72,37 +90,45 @@ def epoch(data, triggers, channels, conditions, max_epochs=100):
             
     return epochs
 
-
 def save_epochs(megdata, epochs, epochs_filtered):
     """ This needs cleaning up to avoid repitition."""
     
     print "Saving epochs ..."
     print shape(epochs)
     
+    #megdata.root.epochs.remove()
+    #megdata.root.epochs_filtered.remove()
+    
     if "/epochs" not in megdata:
-        megdata.createCArray(megdata.root, "epochs", tables.Float32Atom(), shape(epochs))
+        megdata.createCArray(
+            where=megdata.root, 
+            name="epochs", 
+            atom=tables.Float32Atom(), 
+            shape=shape(epochs),
+            filters=tables.Filters(1))
         
     if "/epochs_filtered" not in megdata:
-        megdata.createCArray(megdata.root, "epochs_filtered", tables.Float32Atom(), shape(epochs_filtered))
+        megdata.createCArray(
+            where=megdata.root,
+            name="epochs_filtered",
+            atom=tables.Float32Atom(),
+            shape=shape(epochs_filtered),
+            filters=tables.Filters(1))
     
     megdata.root.epochs[:] = epochs
     megdata.root.epochs_filtered[:] = epochs_filtered
     
     print "Done saving ..."
 
-
-def baseline(epochs):
+def baseline(epochs, epochs_pre):
     print "Baselining ..."
     for condition in arange(shape(epochs)[0]):
         for channel in arange(shape(epochs)[2]):
-            epochs[condition, :, channel, :] = epochs[condition, :, channel, :] - mean(epochs[condition, 0:100, channel, :], 0)
+            epochs[condition, :, channel, :] = epochs[condition, :, channel, :] - mean(epochs[condition, 0:epochs_pre, channel, :], 0)
     return epochs
                 
 def rms(data, axis=0): 
     return sqrt(mean(data ** 2, axis))
-
-
-    
 
 def plot_rms_mean_conditions(mean_epochs):
     for condition in range(8):
