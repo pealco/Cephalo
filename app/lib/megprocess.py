f60c6fafe71b76968be8dbeb158572370b9d6e0d
Pedro Alcocer <pealco@gmail.com>
2009-11-20 14:51:31 -0500

    Rming a lot of the code that was moved into `cephalo.py`.

--- app/lib/megprocess.py
+++ app/lib/megprocess.py
@@ -5,7 +5,6 @@ import tables
 from epoch_rejection import *
 from peak_finding import *
 
-
 def find_triggers(data, trigger_channels):
     """Locates triggers in the trigger channels. Returns a list of arrays."""
     
@@ -18,85 +17,13 @@ def find_triggers(data, trigger_channels):
     
     return triggers
 
-def epoch(data, triggers, channels, config, name="epochs"):
-    """Pulls out the epochs from the long data file."""
-    
-    conditions = config.num_of_conditions
-    expected_epochs = config.expected_epochs
-    stimulus_pre = config.epoch_pre
-    stimulus_post = config.epoch_post
-    
-    epoch_length  = stimulus_pre + 1 + stimulus_post # Add 1 for 0 point.
-    
-    epochs = zeros((conditions, epoch_length, len(channels), expected_epochs))
-    
-    for (channel_index, channel) in enumerate(channels):
-        print "Epoching channel %s ..." % channel
-        the_chan = data[channel][:]
-        for condition in range(conditions): 
-            epoch_indices = range(len(triggers[condition]))
-            random.shuffle(epoch_indices)
-            epoch_indices = epoch_indices[:expected_epochs]
-            
-            for i, t in enumerate(epoch_indices):
-                epoch_start = triggers[condition][t] - stimulus_pre
-                epoch_end   = triggers[condition][t] + stimulus_post
-                the_epoch = the_chan[range(epoch_start, epoch_end + 1)]
-                epochs[condition, :, channel_index, i] = the_epoch
-    
-    
-    
-    ## This should work in the next version of pytables. Uses fancy indexing.   
-    #for condition in conditions: 
-    #    print "Epoching condition %s ..." % condition
-    #    for t in range(max_epochs):
-    #        epoch_start = triggers[condition][t] - stimulus_pre
-    #        epoch_end   = triggers[condition][t] + stimulus_post
-    #        the_epoch = data[channels][range(epoch_start, epoch_end + 1)]
-    #        epochs[condition, :, :, t] = the_epoch
-            
-            
-    return epochs
-
-def save_epochs(megdata, epochs, epochs_filtered):
-    """ This needs cleaning up to avoid repitition."""
-    
-    print "Saving epochs ..."
-    print shape(epochs)
-    
-    #megdata.root.epochs.remove()
-    #megdata.root.epochs_filtered.remove()
-    
-    if "/epochs" not in megdata:
-        megdata.createCArray(
-            where=megdata.root, 
-            name="epochs", 
-            atom=tables.Float32Atom(), 
-            shape=shape(epochs),
-            filters=tables.Filters(1))
-        
-    if "/epochs_filtered" not in megdata:
-        megdata.createCArray(
-            where=megdata.root,
-            name="epochs_filtered",
-            atom=tables.Float32Atom(),
-            shape=shape(epochs_filtered),
-            filters=tables.Filters(1))
-    
-    megdata.root.epochs[:] = epochs
-    megdata.root.epochs_filtered[:] = epochs_filtered
-    
-    print "Done saving ..."
-
 def baseline(epochs, epochs_pre):
     print "Baselining ..."
     for condition in arange(shape(epochs)[0]):
         for channel in arange(shape(epochs)[2]):
             epochs[condition, :, channel, :] = epochs[condition, :, channel, :] - mean(epochs[condition, 0:epochs_pre, channel, :], 0)
     return epochs
-                
-def rms(data, axis=0): 
-    return sqrt(mean(data ** 2, axis))
+    
 
 def plot_rms_mean_conditions(mean_epochs):
     for condition in range(8):
