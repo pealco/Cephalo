1ef6db084d262280ab7fea78be8f40edd8dd66c4
Pedro Alcocer <pealco@gmail.com>
2009-12-01 14:12:09 -0500

    Reorganizing for packaging.

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 #! /usr/bin/env python2.5
 # -*- coding: utf-8 -*-
 
-__author__ = 'Pedro Alcocer, pealco@gmail.com'
- 
- 
-from setuptools import setup, find_packages
- 
+from distutils.core import setup
+from setuptools import setup
+from cephalo import __version__
+
  
 setup(
-    name="Cephalo",
-    version="0.1",
-    description="Cephalo is a framework for the fast prototyping of MEG data analyses.",
-    license="GPL",
-    keywords="MEG, neuroscience, data analysis, scientific computing",
-    url="http://pealco.net",
-    packages=find_packages(exclude=['examples', 'docs']),
+    name            = "Cephalo",
+    version         = __version__,
+    description     = "Cephalo is a framework for the fast prototyping of MEG data analyses.",
+    keywords        = "MEG, neuroscience, data analysis, scientific computing",
+    license         = "GPL3",
+    author          = "Pedro Alcocer",
+    author_email    = "pealco@gmail.com",
+    url             = "http://pealco.net/cephalo",
+    
+    classifiers     = [ 
+                'Development Status :: 1 - Alpha',
+                'Intended Audience :: End Users/Desktop',
+                'Intended Audience :: Science/Research',
+                'Natural Language :: English',
+                'Operating System :: POSIX',
+                'Programming Language :: Python'
+    ]
+    packages        = ["sqd2h5", "cephalo"]
+    scripts         = ["sqd2h5", "cephalo"]
+    
+    packages        = find_packages(exclude=['examples', 'docs']),
     include_package_data=True,
     install_requires=['numpy','tables']
     #test_suite='pybrain.tests.runtests.make_test_suite',
