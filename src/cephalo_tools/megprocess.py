f6ceabf884c005f3c1dc5014c63a050780b12987
Pedro Alcocer <pealco@gmail.com>
2010-05-14 17:58:24 -0400

    find_triggers is rewritten. Should solve some problems which finding too many triggers.

--- src/cephalo_tools/megprocess.py
+++ src/cephalo_tools/megprocess.py
@@ -1,26 +1,77 @@
 """Some utility functions specific to MEG data."""
 
-from numpy import mean, nonzero, diff, arange, shape
-from pylab import plot, figure
+from numpy import mean, nonzero, diff, arange, shape, array, amax, amin, size
+from pylab import plot, figure, find
 from utility import rms
 #from pylab import *
 #from peak_finding import *
 
-def find_triggers(data, trigger_channels):
+def find_triggers(data, triggerLinesPotential=None, triggerHighThresh=None, triggerLowThresh=None):
     """Locates triggers in the trigger channels. Returns a list of arrays."""
     
-    threshold = (25-1) / float(5 * 2)
-    triggers = []
-    for trigger_channel in trigger_channels:
-        triggers.append(nonzero(diff(data[trigger_channel, :]) > threshold)[0])
+    if not triggerLinesPotential:
+        triggerLinesPotential = arange(160, 192)
         
-    for index in range(len(triggers)):
-        triggers[index] = triggers[index][triggers[index] > 100]
-        trigger_pairs = zip(triggers[index], triggers[index][1:])
-        triggers[index] = [trigger_pair[0] for trigger_pair in trigger_pairs 
-            if diff(trigger_pair) > 100]
+    if not triggerHighThresh:
+        triggerHighThresh = 5 # mV
     
-    return triggers
+    if not triggerLowThresh:
+        triggerLowThresh = 0.2 # mV
+        
+    triggerHighThresh_mV = triggerHighThresh * 1000 # mV
+    triggerLowThresh_mV  = triggerLowThresh  * 1000 # mV
+    
+    triggerData = f.root.raw_data[160:192, :].T * f.root.convfactor[160:192]
+    
+    # First find channels whose maximum is at least the "high" threshold and
+    # whose minimum is at most the "low" threshold.
+    
+    triggerLinesTest = ((amax(triggerData, 0) > triggerHighThresh_mV) & (amin(triggerData, 0) < triggerLowThresh_mV))
+    triggerDataProbable = triggerData[:,triggerLinesTest]
+    triggerLinesProbable = triggerLinesPotential[triggerLinesTest]
+    #del triggerData
+    
+    triggersNearHigh = mean(triggerDataProbable > triggerHighThresh_mV, 0)
+    triggersNearLow = mean(triggerDataProbable < triggerLowThresh_mV, 0)
+    
+    triggerLinesHealthyHighRest =  (triggersNearHigh > 25 * triggersNearLow)
+    triggerDataHighRestGood = triggerDataProbable[:,triggerLinesHealthyHighRest]
+    triggerLinesHighRestGood = triggerLinesProbable[triggerLinesHealthyHighRest]
+    triggerLinesHighRestGoodCount = size(triggerLinesHighRestGood)
+    
+    triggerLinesHealthyLowRest =  (triggersNearLow > 25 * triggersNearHigh)
+    triggerDataLowRestGood = triggerDataProbable[:,triggerLinesHealthyLowRest]
+    triggerLinesLowRestGood = triggerLinesProbable[triggerLinesHealthyLowRest]
+    triggerLinesLowRestGoodCount = size(triggerLinesLowRestGood)
+    
+    if triggerLinesHighRestGoodCount > triggerLinesLowRestGoodCount:
+        triggerDataGood = triggerDataHighRestGood
+        triggerLinesGood = triggerLinesHighRestGood
+        triggerLinesGoodCount = triggerLinesHighRestGoodCount
+    else:
+        triggerDataGood = triggerDataLowRestGood
+        triggerLinesGood = triggerLinesLowRestGood
+        triggerLinesGoodCount = triggerLinesLowRestGoodCount
+        
+    # del triggerDataProbable
+    
+    triggersList = []
+    
+    for iTrigger in range(triggerLinesGoodCount):
+        if triggerLinesHighRestGoodCount > triggerLinesLowRestGoodCount:
+            triggerSet = find(triggerDataGood[:, iTrigger] < triggerLowThresh_mV)
+        else:
+            triggerSet = find(triggerDataGood[:, iTrigger] > triggerHighThresh_mV)
+
+        # the next line uses find and diff to discard samples of triggerSet
+        # that immediately follow any other values that cross
+        # the "set" threshold.
+        
+        triggerSamples = (triggerSet[hstack((0, find(diff(triggerSet) > 1)+1))])
+        triggersList.append(triggerSamples)
+    
+    return triggerSamples
+    
 
 def baseline(epochs, epochs_pre):
     """Performs baselining. Averages the epoch before the stimulus and subtracts
